057cbbd83a90520d6fe00e4b7d97e6252e6c82e5
Liav <liavcohen955@gmail.com>
2024-08-17 10:52:58 +0300

    first commit

---
+++ client/src/components/UrlInput.jsx
@@ -0,0 +1,21 @@
+// src/UrlInput.js
+import React from 'react';
+import "../styles/url.css"
+function UrlInput({ index, value, onChange, onDelete, isValid }) {
+  return (
+    <div className="url-input-container">
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => onChange(index, e.target.value)}
+        placeholder={`URL ${index + 1}`}
+        className={`url-input ${isValid ? '' : 'invalid'}`}
+      />
+      <button type="button" onClick={() => onDelete(index)} className="delete-button">
+        Delete
+          </button>
+      </div>
+  );
+}
+
+export default UrlInput;
