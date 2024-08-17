aa81a6650b4bf2fa045f8520f550efe1e2236e0f
Liav <liavcohen955@gmail.com>
2024-08-17 12:10:49 +0300

    add trust proxy

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,8 @@ const rateLimit = require("express-rate-limit");
 const cors = require("cors");
 
 const app = express();
+app.set("trust proxy", 1);
+
 app.use(cors());
 app.use(express.json());
 app.use(helmet());
